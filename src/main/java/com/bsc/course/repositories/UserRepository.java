dab4dbed1942e7a24e7ced53b783a1dcdcc5bc92
Bruno Souza <brunosouzacvlho@gmail.com>
2023-06-05 20:08:40 -0300

    Service layer, component registration

--- src/main/java/com/bsc/course/repositories/UserRepository.java
+++ src/main/java/com/bsc/course/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.bsc.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bsc.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	
-	
 }
